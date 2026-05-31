4e331115ab89d1ac9f3d4b9b65473c38e31d32b6
Daniel Chamorro <55163728+dchamorro1@users.noreply.github.com>
2026-05-31 18:36:59 -0500

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        maxSell = 0
+        minBuy = prices[0]
+
+        # our minBuy is initialized to prices[0]
+
+        # iterate through each value in prices. At each stage we calcualte the maxSell and the Minimum Buy.
+        # keep highestMaxSell and return it at the end
+
+        for price in prices:
+            maxSell = max(maxSell, price - minBuy)
+            minBuy = min(minBuy, price)
+
+        return maxSell
