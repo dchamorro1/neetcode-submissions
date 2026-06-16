4d4871e6f9baa9a1d9caf7b6ff5eb747d6395860
Daniel Chamorro <55163728+dchamorro1@users.noreply.github.com>
2026-06-16 14:43:57 -0500

    Add: climbing-stairs - submission-8

---
+++ Data Structures & Algorithms/climbing-stairs/submission-8.py
@@ -0,0 +1,22 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        waysToGetToPrevPrev = 1
+        waysToGetToPrev = 1
+
+        # if n = 3, we need to calcualte
+        # ways to get to 0, 1, 2, 3
+        # we already know ways to get to 0, and 1
+
+        # n will be 0 and 1 which is 2 times
+        # this gives us ways to get to 2 and 3 that we are missing
+
+        for i in range(n-1):
+            temp = waysToGetToPrev
+
+            waysToGetToPrev = waysToGetToPrevPrev + waysToGetToPrev 
+            waysToGetToPrevPrev = temp
+
+        return waysToGetToPrev
+
+        # Time: O(n)
+        # Space: O(1)
